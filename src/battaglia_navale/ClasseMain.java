78f5a90898100db7781024941a441c844798405c
FlamingoBike <36803440+FlamingoBike@users.noreply.github.com>
2019-02-11 12:39:15 +0100

    Update ClasseMain.java

--- src/battaglia_navale/ClasseMain.java
+++ src/battaglia_navale/ClasseMain.java
@@ -4,7 +4,7 @@ public class ClasseMain {
 
 	public static void main(String[] args) {
 		System.out.println("Test git");
-		System.out.println("Secondo push");
+		System.out.println("Secondo push aaaaaa");
 	}
 
 }
